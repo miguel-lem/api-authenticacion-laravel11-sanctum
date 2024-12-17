973bf19d9f928409abe3b46e2ab32b3eed764a19
Miguel Lema <mlema@uioromerodya.com>
2024-12-17 14:25:31 -0500

    segundo commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -26,13 +26,30 @@ class ProductController extends Controller
     /**
      * Show the form for creating a new resource.
      */
+    //funcion para crear un producto
     public function create(Request $request)
     {
         //
         $validator = Validator::make($request->all(),[
             'name'=>'required|string',
             'description'=>'required|string',
-            'price'=>'required|float'
+            'price'=>'required'
+        ]);
+
+        if($validator->fails()){
+            return Response()->json([
+                'message'=>'Los datos no estan correctos',
+                'error'=>$validator->errors()
+            ]);
+        }
+        $product = Product::create([
+            'name'=>$request->name,
+            'description'=>$request->description,
+            'price'=>$request->price
+        ]);
+        return Response()->json([
+            'message'=>'el producto fue creado',
+            'result'=>$product
         ]);
     }
 
@@ -47,25 +64,93 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Product $product)
+    public function show($id)
     {
         //
+        $product = Product::find($id);
+        if(!$product){
+            return Response()->json([
+                'message'=>'Producto no encontrado',
+                'status'=>201
+            ]);
+        }else{
+            return Response()->json([
+                'message'=>'pruducto encontrado',
+                'respuesta'=>$product
+            ]);
+        }
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Product $product)
+    public function delete($id)
     {
         //
+        $product = Product::find($id);
+
+        if (!$product) {
+            $data = [
+                'message' => 'Producto no encontrado',
+                'status' => 404
+            ];
+            return response()->json($data, 404);
+        }
+        
+        $product->delete();
+
+        $data = [
+            'message' => 'Producto eliminado',
+            'status' => 200
+        ];
+
+        return response()->json($data, 200);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Product $product)
+    public function update(Request $request, $id)
     {
         //
+        $product = Product::find($id);
+
+        if (!$product) {
+            $data = [
+                'message' => 'Producto no encontrado',
+                'status' => 404
+            ];
+            return response()->json($data, 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name'=>'required|string',
+            'description'=>'required|string',
+            'price'=>'required'
+        ]);
+
+        if ($validator->fails()) {
+            $data = [
+                'message' => 'Error en la validación de los datos',
+                'errors' => $validator->errors(),
+                'status' => 400
+            ];
+            return response()->json($data, 400);
+        }
+
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->price = $request->price;
+
+        $product->save();
+
+        $data = [
+            'message' => 'Producto actualizado',
+            'student' => $product,
+            'status' => 200
+        ];
+
+        return response()->json($data, 200);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -9,12 +9,20 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+//rutas para inicio de sesion
 Route::get('/products',[ProductController::class,'index']);
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
 
 
+//grupo de rutas protegidas
 Route::middleware(['auth:sanctum'])->group(function(){
     Route::get('/logout',[AuthController::class,'logout']);
     Route::get('/products',[ProductController::class,'index']);
+    Route::post('/create',[ProductController::class,'create']);
+    Route::get('/show/{id}',[ProductController::class,'show']);
+    Route::delete('/delete/{id}',[ProductController::class,'delete']);
+    Route::post('/update/{id}',[ProductController::class,'update']);
 });
+
+
